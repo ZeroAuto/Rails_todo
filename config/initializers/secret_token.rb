80fa221e376fbb0a01c37baa06bda7f444361078
Michael Cooley <mikejames386@gmail.com>
2014-03-24 15:27:07 -0500

    create scaffold for lists

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TODO::Application.config.secret_key_base = 'fd5ca6dbbc1d5cecaf8dd64b61b2b09af7b666fd16cf96521e53db4c74275e0c991150720df263de04c534f29fe23947747ab8fd9051eed273c29369e17c5d6b'
