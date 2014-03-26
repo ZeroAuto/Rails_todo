0b721feee4eb73b121c81806806bc1c4be21be8c
Michael Cooley <mike@jackis-airport-extreme.gateway.2wire.net>
2014-03-26 12:05:38 -0500

    rename index buttons and add reject if black to list model

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,4 @@
 class List < ActiveRecord::Base
 	has_many :items
-	accepts_nested_attributes_for :items, allow_destroy: true
+	accepts_nested_attributes_for :items, allow_destroy: true, :reject_if => lambda { |c| c[:name].blank? }
 end
