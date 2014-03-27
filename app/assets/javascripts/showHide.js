772f32b86d971de8a610404feca890ac71bc2451
Michael Cooley <mikejames386@gmail.com>
2014-03-27 12:31:43 -0500

    change new list view

--- app/assets/javascripts/showHide.js
+++ app/assets/javascripts/showHide.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
-	$(".remove_nested_fields_link").hide();
-	$(".show_remove_button").click(function(){
-	  $(this).closest(".show_remove_button").children(".remove_nested_fields_link").toggle();
+	$(".hidden_button").hide();
+	$(".shown_button").click(function(){
+	  $(this).next().show();
+	  console.log(this);
+	  $(this).remove();
 	});
 })
