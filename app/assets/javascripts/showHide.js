aaae13c2b857e7a66d7bd458da4af2d578f4134c
Michael Cooley <mike@jackis-airport-extreme.gateway.2wire.net>
2014-03-27 12:46:11 -0500

    change showHides.js

--- app/assets/javascripts/showHide.js
+++ app/assets/javascripts/showHide.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
 	$(".hidden_button").hide();
 	$(".shown_button").click(function(){
 	  $(this).next().show();
-	  console.log(this);
-	  $(this).remove();
+	  // $(this).remove();
 	});
 })
